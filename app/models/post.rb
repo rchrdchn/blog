d35fc3ba9ca7400d0ea26fbf94efd4c923763e63
Richard Chan <richardchanme@gmail.com>
2020-04-15 22:25:10 -0700

    Added validation for post title and body; showing error message on post view

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
     # belongs_to :user
     # has_many :comments
+
+    validates :title, presence: true, length: { minimum: 3 }
+    validates :body, presence: true, length: { minimum: 3 }
 end
