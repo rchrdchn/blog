4c0e3a3894f2c72edb1912d301bcc4d583955650
Richard Chan <richardchanme@gmail.com>
2020-04-15 23:20:15 -0700

    Added Comment Model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_051519) do
+ActiveRecord::Schema.define(version: 2020_04_16_061744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2020_04_14_051519) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -83,5 +92,6 @@ ActiveRecord::Schema.define(version: 2020_04_14_051519) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "services", "users"
 end
