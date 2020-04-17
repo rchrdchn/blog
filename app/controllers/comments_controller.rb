28b427e6646b3c20e887724597e44f5494632a4a
Richard Chan <richardchanme@gmail.com>
2020-04-16 22:53:48 -0700

    Added Destroy action on Comments Controller; added destroy dependents on Post controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,14 @@ class CommentsController < ApplicationController
     def create
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
-        
-        if @comment.errors.any?
-            render "posts/show"
-        else
-            redirect_to post_path(@post)
-        end
+        redirect_to post_path(@post)
+    end
+
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to post_path(@post)
     end
 
     private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
     # belongs_to :user
-    has_many :comments
+    has_many :comments, dependent: :destroy
 
     validates :title, presence: true, length: { minimum: 3 }
     validates :body, presence: true, length: { minimum: 3 }
