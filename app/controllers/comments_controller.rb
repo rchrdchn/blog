00473ca0851044289a34bcc4707573c50b4df606
Richard Chan <richardchanme@gmail.com>
2020-04-16 22:47:06 -0700

    Added Create Comments action on Controller; validation fields on Model; rendering comments on Post view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(comment_params)
+        
+        if @comment.errors.any?
+            render "posts/show"
+        else
+            redirect_to post_path(@post)
+        end
+    end
+
+    private
+        def comment_params
+            params.require(:comment).permit(:commenter, :body)
+        end
 end
