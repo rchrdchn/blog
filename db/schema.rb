ffedf202ea261224ab17fef6f0076a2672af3141
Richard Chan <richardchanme@gmail.com>
2020-04-17 21:28:32 -0700

    Created associations between User and Post, and migrations for foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_061744) do
+ActiveRecord::Schema.define(version: 2020_04_18_041833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 2020_04_16_061744) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "services", force: :cascade do |t|
@@ -93,5 +95,6 @@ ActiveRecord::Schema.define(version: 2020_04_16_061744) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "users"
   add_foreign_key "services", "users"
 end
