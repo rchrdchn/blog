5d83f72018d178dae8a1804c1c6cb9d16f0b267d
Richard Chan <richardchanme@gmail.com>
2020-04-17 22:32:43 -0700

    User can edit comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,31 @@
 class CommentsController < ApplicationController
+    def index
+        @comments = Comment.all
+    end
+    
     def create
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
         redirect_to post_path(@post)
     end
+
+    def edit
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+
+        if @comment.update(comment_params)
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+    
+    
 
     def destroy
         @post = Post.find(params[:post_id])
