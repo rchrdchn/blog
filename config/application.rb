00762ea8ec2f82eb6219abe6458b415d8fa7e491
Richard Chan <richardchanme@gmail.com>
2020-04-24 15:35:56 -0700

    Updated gems and did yarn install

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Blog
     config.active_job.queue_adapter = :sidekiq
     config.application_name = Rails.application.class.parent_name
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
